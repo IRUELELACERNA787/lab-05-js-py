b074c7b2c00c95066e421b61c757d15e206b5e2b
Kai Lacerna <iruelelacerna787>
2022-03-07 16:51:37 -0500

    +madlibs python version

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+noun = input("Please enter a Noun: ")
+adjective = input("Please enter an Adjective: ")
+adverb = input("Please enter an Adverb: ")
+verb = input("Please enter a Verb: ")
+
+print("On a tropical "+ noun +",")
+print("Underneath a " + adverb + " " + adjective + " moon.")
+print(verb + " with the hula dancers,")
+print("Askin' questions cause they got all the answers.")
